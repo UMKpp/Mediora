4e6c78a2394bd4339de5d09df017093b711a458c
K.A.U.M.Kuruppu <upumk2003@gmail.com>
2026-06-26 12:25:56 +0530

    Create page.js

---
+++ app/dashboard/settings/page.js
@@ -0,0 +1,11 @@
+import DashboardPlaceholder from "../DashboardPlaceholder";
+
+export default function SettingsPage() {
+  return (
+    <DashboardPlaceholder
+      icon="settings"
+      title="Settings"
+      description="This area will hold dashboard preferences and account settings after the user profile flow is finalized."
+    />
+  );
+}
