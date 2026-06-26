851ec04144bf8c6da20ad9a518585451a7a4e913
K.A.U.M.Kuruppu <upumk2003@gmail.com>
2026-06-26 13:30:56 +0530

    Create page.js

---
+++ app/dashboard/pharmacies/page.js
@@ -0,0 +1,11 @@
+import DashboardPlaceholder from "../DashboardPlaceholder";
+
+export default function PharmaciesPage() {
+  return (
+    <DashboardPlaceholder
+      icon="pharmacy"
+      title="Pharmacies"
+      description="This area will show nearby pharmacies and medicine access information after location and pharmacy data are connected."
+    />
+  );
+}
