cb4d6942c267b09c3fdf9ebbe5557e3febe32464
K.A.U.M.Kuruppu <upumk2003@gmail.com>
2026-06-26 13:31:10 +0530

    Create page.js

---
+++ app/dashboard/doctors/page.js
@@ -0,0 +1,11 @@
+import DashboardPlaceholder from "../DashboardPlaceholder";
+
+export default function DoctorsPage() {
+  return (
+    <DashboardPlaceholder
+      icon="doctor"
+      title="Doctors"
+      description="This area will help users find doctors, view care options, and prepare for appointments when provider data is added."
+    />
+  );
+}
