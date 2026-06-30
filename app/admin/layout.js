f911e16e5b4cbf704e374d20e187064f28018afc
K.A.U.M.Kuruppu <upumk2003@gmail.com>
2026-06-30 08:35:15 +0530

    Create layout.js

---
+++ app/admin/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Mediora Admin",
+  description: "Mediora administrative dashboard",
+};
+
+export default function AdminLayout({ children }) {
+  return children;
+}
